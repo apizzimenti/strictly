1b432a0a11c671f25d1c3153bfdff67c9a14353e
apizzimenti <pizzimentianthony@gmail.com>
2016-06-24 15:12:02 -0500

    edit changed config information, globbing

--- tasks/strictly.js
+++ tasks/strictly.js
@@ -13,15 +13,15 @@ module.exports = function (grunt) {
 		
 		var done = this.async();
 
-		grunt.config.requires("strictly.config.files");
+		grunt.config.requires("strictly.files");
 
-		var files = grunt.config("strictly.config.files"),
+		var files = grunt.config("strictly.files"),
 			cwd = grunt.config("strictly.config.cwd") || process.cwd(),
 			lines = grunt.config("strictly.config.liens") || 10,
-			paths = Array.isArray(files) ? files : [files],
+			paths = c.getGlob(files),
 			num = paths.length,
 			checked = 0;
-		
+
 		paths.forEach(function (p) {
 			c.checker(grunt, p, done, cwd, checked, num, lines);
 		});
