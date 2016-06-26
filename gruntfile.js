8bf1a212c7f4690e8ede6a61751f47d9ce353aca
apizzimenti <pizzimentianthony@gmail.com>
2016-06-26 12:47:29 -0500

    test some more stuff out

--- gruntfile.js
+++ gruntfile.js
@@ -3,32 +3,26 @@
  * Created by apizzimenti on 6/23/16.
  */
 
-var grunt = require("grunt");
-
 module.exports = function (grunt) {
 	grunt.loadTasks("./tasks");
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 
 	grunt.initConfig({
-		
-		strictly: {
-			config: {
-				cwd: "test/"
-			},
-			files: [
-				"**/*.js"
-			]
-		},
 
 		jshint: {
-			all: ["test/testjs.js"]
+			files: {
+				src: ["test/testjs.js"]
+			}
+		},
+
+		strictly: {
+			files: ["test/**/*.js"]
 		}
 	});
 
 	grunt.registerTask("default", [
-		"jshint",
-		"strictly"
+		"strictly",
+		"jshint"
 	]);
 
-	grunt.registerTask("jshint", ["jshint"]);
 };
