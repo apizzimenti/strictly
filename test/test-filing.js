4bfb67fb217451d3a1c888117ac339898d76661f
apizzimenti <pizzimentianthony@gmail.com>
2016-06-28 21:42:40 -0500

    testing

---
+++ test/test-filing.js
@@ -0,0 +1,21 @@
+/**
+ * Created by apizzimenti on 6/28/16.
+ */
+
+var assert = require("assert"),
+	filing = require("../tasks/filing");
+
+describe("filing", () => {
+
+	var test_path = "./test-files/**/*.js",
+		files;
+
+	describe("#glob()", () => {
+		it("returns an array of files", () => {
+			files = filing.getGlob(test_path);
+			assert(Array.isArray(files));
+		});
+	});
+
+	// more test code on the way
+});
